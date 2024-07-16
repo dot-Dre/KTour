2c9572b2fbe875e7ab6960bc31d80325cd101763
Andre Lepardo <lepardandr@ecs.vuw.ac.nz>
2024-07-17 11:04:11 +1200

    Implemented piece model

--- src/components/piece.jsx
+++ src/components/piece.jsx
@@ -1,37 +1,44 @@
 import * as React from "react";
 import { useRef, useState, useEffect } from "react";
 import { useFrame } from "@react-three/fiber";
+import { useGLTF } from "@react-three/drei";
 import CanvasProps from "../canvasProps";
+import gltfFile from "./chessknight.glb";
 
 const Piece = ({ position, onReached }) => {
-    const ref = useRef();
-    const [currentPos, setCurrentPos] = useState(position);
-  
-    useEffect(() => {
-      setCurrentPos(position);
-    }, [position]);
-  
-    useFrame(() => {
-      if (ref.current) {
-        const targetPosition = {
-          x: currentPos[1] * CanvasProps.squareSize + CanvasProps.boardOrigin.x,
-          y: 1,
-          z: currentPos[0] * CanvasProps.squareSize + CanvasProps.boardOrigin.z,
-        };
-        ref.current.position.lerp(targetPosition, CanvasProps.animationSpeed);
-        const distance = ref.current.position.distanceTo(targetPosition);
-        if (distance < 0.1) {
-          onReached(currentPos);
-        }
+  const ref = useRef();
+  const { nodes, materials } = useGLTF(gltfFile);
+  const [currentPos, setCurrentPos] = useState(position);
+
+  useEffect(() => {
+    setCurrentPos(position);
+  }, [position]);
+
+  useFrame(() => {
+    if (ref.current) {
+      const targetPosition = {
+        x: currentPos[1] * CanvasProps.squareSize + CanvasProps.boardOrigin.x,
+        y: 1,
+        z: currentPos[0] * CanvasProps.squareSize + CanvasProps.boardOrigin.z,
+      };
+      ref.current.position.lerp(targetPosition, CanvasProps.animationSpeed);
+      const distance = ref.current.position.distanceTo(targetPosition);
+      if (distance < 0.1) {
+        onReached(currentPos);
       }
-    });
-  
-    return (
-      <mesh ref={ref} castShadow>
-        <sphereGeometry args={[0.5, 32, 32]} />
-        <meshStandardMaterial color={0xff0000} metalness={0.5} roughness={0.5} />
-      </mesh>
-    );
-  };
-  
-  export default Piece;
+    }
+  });
+
+  return (
+    <mesh
+      ref={ref}
+      castShadow
+      receiveShadow
+      rotation={[-Math.PI / 2, 0, 0]}
+      geometry={nodes.Cylinder__0.geometry}
+      material={materials["Scene_-_Root"]}
+    />
+  );
+};
+
+export default Piece;
