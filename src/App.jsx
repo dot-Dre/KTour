43bd99abd3cbbe827cadb5e13077ffcd9c1dd76e
Andre Lepardo <lepardandr@ecs.vuw.ac.nz>
2024-07-16 16:55:20 +1200

    Built scaffold of main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from "react";
+import { Canvas } from "@react-three/fiber";
+import Typewriter from "typewriter-effect";
 
 const App = () => {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
-  )
-}
+    <section className="home-sec w-full h-screen flex flex-col justify-between">
+      <div className="text-8xl pt-8 pl-6 font-bold text-black">
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .typeString("Knight's Tour ♞")
+              .changeDelay(10)
+              .pauseFor(2500)
+              .start();
+          }}
+        />
+      </div>
+      <Canvas
+        camera={{ position: [20, 20, 20], fov: 50 }}
+        className="flex-grow"
+      />
+      <div className="absolute bottom-0 inset-x-0 flex justify-center p-6 bg-white shadow-lg">
+        <input
+          type="text"
+          placeholder="Enter starting cell"
+          className="py-2 px-4 border border-gray-300 rounded-l focus:outline-none"
+        />
+        <button className="py-2 px-4 bg-blue-500 text-white rounded-r hover:bg-blue-600 focus:outline-none">
+          Solve
+        </button>
+      </div>
+    </section>
+  );
+};
 
-export default App
+export default App;
